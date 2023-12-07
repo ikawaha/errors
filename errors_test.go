6490953a0303a1fe0a646a9cf46943d604d0ec24
ikawaha <ikawaha@users.noreply.github.com>
2023-12-08 00:11:13 +0900

    chore: Update

--- errors_test.go
+++ errors_test.go
@@ -1,17 +1,11 @@
 package errors_test
 
 import (
-	stderrs "errors"
 	"fmt"
 	"testing"
 
 	"github.com/ikawaha/errors"
 )
-
-// standard error
-func errFn0() error {
-	return stderrs.New("error caused by f0")
-}
 
 // error with the stack trace
 func errFn1() error {
@@ -21,11 +15,6 @@ func errFn1() error {
 // fn1 -> fn2
 func errFn2() error {
 	return fmt.Errorf("fn2 has error: %w", errFn1())
-}
-
-// fn1 -> fn2 -> fn3
-func errFn3() error {
-	return errors.Errorf("fn3 has error: %w", errFn2())
 }
 
 func TestNew(t *testing.T) {
@@ -105,67 +94,6 @@ func TestChain(t *testing.T) {
 				if got := err.Error(); tt.wantErr != got {
 					t.Errorf("want: %q, got: %q", tt.wantErr, got)
 				}
-			}
-		})
-	}
-}
-
-func TestErrorf(t *testing.T) {
-	type args struct {
-		format string
-		a      []any
-	}
-	tests := []struct {
-		name    string
-		args    args
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if err := errors.Errorf(tt.args.format, tt.args.a...); (err != nil) != tt.wantErr {
-				t.Errorf("Errorf() error = %v, wantErr %v", err, tt.wantErr)
-			}
-		})
-	}
-}
-
-func TestStackTrace(t *testing.T) {
-	type args struct {
-		err error
-	}
-	tests := []struct {
-		name string
-		args args
-		want string
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := errors.StackTrace(tt.args.err); got != tt.want {
-				t.Errorf("StackTrace() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func TestWithStacktrace(t *testing.T) {
-	type args struct {
-		err error
-	}
-	tests := []struct {
-		name    string
-		args    args
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if err := errors.WithStacktrace(tt.args.err); (err != nil) != tt.wantErr {
-				t.Errorf("WithStacktrace() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
 	}
