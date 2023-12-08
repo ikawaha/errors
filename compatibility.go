0f47315f20fedb2f8a4595b5e47feb4f59f9e5e3
ikawaha <ikawaha@users.noreply.github.com>
2023-12-08 12:23:46 +0900

    chore: Adjustment

--- compatibility.go
+++ compatibility.go
@@ -6,26 +6,33 @@ import (
 	"github.com/ikawaha/errors/contexter"
 )
 
+// As is provided for compatibility with errors.As.
 func As(err error, target any) bool {
 	return stderrs.As(err, target)
 }
 
+// Is is provided for compatibility with errors.Is.
 func Is(err, target error) bool {
 	return stderrs.Is(err, target)
 }
 
+// Join is provided for compatibility with errors.Join.
 func Join(errs ...error) error {
 	return stderrs.Join(errs...)
 }
 
+// New is provided for compatibility with errors.New.
+// It captures the stack trace.
 func New(text string) error {
 	ret := stderrs.New(text)
-	if captureStackTrace {
-		ret = contexter.WithStackTrace(ret)
+	if stacktraceCapture {
+		ret = contexter.WithStacktrace(ret, 1)
 	}
 	return ret
 }
 
+// Unwrap is provided for compatibility with errors.Unwrap.
+// In particular Unwrap does not unwrap errors returned by Join or Chain.
 func Unwrap(err error) error {
 	return stderrs.Unwrap(err)
 }
