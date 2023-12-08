fdc956f85bc77ba0ce9ec4c29d3283aca325cb5e
ikawaha <ikawaha@users.noreply.github.com>
2023-12-08 10:10:36 +0900

    cho: Add package infomation

---
+++ contexter/doc.go
@@ -0,0 +1,5 @@
+// Package contexter offers a method to attach simple key/value information to errors,
+// with the special capability of attaching stack traces. Developers can utilize this package
+// to add both additional context and detailed stack trace information to errors,
+// enhancing their informativeness and ease of debugging.
+package contexter
