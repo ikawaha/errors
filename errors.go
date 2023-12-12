a9641c10c241cf821895980c51e3837d2eb84048
ikawaha <ikawaha@users.noreply.github.com>
2023-12-12 22:13:52 +0900

    feat: Add the WithErrors method

--- errors.go
+++ errors.go
@@ -48,12 +48,27 @@ func Wrapf(err error, format string, a ...any) error {
 	return ret
 }
 
-// NewWithErrors creates new error of text and joins remaining errors, but the Error method of the joined error is
-// the same as the head of the list. All information of the joined error is retained, so the information can be
-// obtained with errors.Is, errors.As.
+// NewWithErrors creates new error of text and joins remaining errors, but the Error
+// method of the joined error is the same as the head of the list. All information of
+// the joined error is retained, so the information can be obtained with errors.Is, errors.As.
 func NewWithErrors(text string, errs ...error) error {
 	arg := make([]error, 0, len(errs)+1)
 	arg = append(arg, stderrs.New(text))
 	arg = append(arg, errs...)
 	return chainer.Chain(arg...)
+}
+
+// WithErrors joins errors of errs to the specific error err.
+// The created error behaves as err, but the joined error can be detected with errors.Is, errors.As.
+// The error specified by err is assumed to be an error not created by this library.
+func WithErrors(err error, errs ...error) error {
+	if err == nil {
+		return nil
+	}
+	ee := make([]error, 0, len(errs)+1)
+	ee = append(ee, err)
+	for _, v := range errs {
+		ee = append(ee, v)
+	}
+	return chainer.Chain(ee...)
 }
--- errors_test.go
+++ errors_test.go
@@ -1,6 +1,7 @@
 package errors_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/ikawaha/errors"
@@ -20,6 +21,32 @@ func TestNewWithErrors(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			err := errors.NewWithErrors(tt.head, tt.tails...)
+			if (err != nil) != tt.want {
+				t.Fatalf("NewWithErrors() error = %v, want not nil", err)
+			}
+			if tt.want {
+				if got := err.Error(); tt.wantErr != got {
+					t.Errorf("want: %q, got: %q", tt.wantErr, got)
+				}
+			}
+		})
+	}
+}
+
+func TestWithErrors(t *testing.T) {
+	tests := []struct {
+		name    string
+		head    error
+		tails   []error
+		want    bool
+		wantErr string
+	}{
+		{name: "head", head: fmt.Errorf("head"), tails: []error{errors.New("t0"), errors.New("t1"), errors.New("t2")}, want: true, wantErr: "head"},
+		{name: "tail has some error including nil", head: fmt.Errorf("head"), tails: []error{nil, errors.New("t0"), errors.New("t1"), errors.New("t2")}, want: true, wantErr: "head"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := errors.WithErrors(tt.head, tt.tails...)
 			if (err != nil) != tt.want {
 				t.Fatalf("NewWithErrors() error = %v, want not nil", err)
 			}
